ff21745daabd41a713dd295062b7223a80d1ea66
AtomicHarshness <155515415+AtomXHarshYT@users.noreply.github.com>
2024-03-22 15:16:53 +0000

    string learned

---
+++ 01_basics/strings.js
@@ -0,0 +1,22 @@
+const name = 'Harshsinh   ';
+const lName = 'Rathod';
+
+console.log(`hello my name is ${name} and surname is ${lName}`);
+
+const looplp = new String(name);
+
+// console.log(looplp);
+// console.log(looplp.__proto__);
+
+// console.log(looplp.length);
+// console.log(looplp.toUpperCase());
+// console.log(looplp.charAt('3'));
+
+
+// console.log(looplp.indexOf('s'));
+// console.log(looplp.slice(0, 2));
+// console.log(looplp.trim());  remove space start / end
+
+// console.log(looplp.replace('har', 'sar'));
+
+console.log(looplp.split('s'));
