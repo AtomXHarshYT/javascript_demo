c443fec3a701c01c7b4313fc2b879d221fec41b5
AtomicHarshness <155515415+AtomXHarshYT@users.noreply.github.com>
2024-03-22 14:50:51 +0000

    head_stack clear

---
+++ stack_Heap_Memory.js
@@ -0,0 +1,21 @@
+// stack (primitive)  heap (non-pri)
+
+let nameOne = 'Harshsinh';
+
+let anotherName = nameOne;
+anotherName = 'harsh rathod';
+
+// console.log(nameOne);
+// console.log(anotherName);
+
+let objOne = {
+    email: 'xyz@gmail.com',
+    id: 101,
+}
+
+let objTwo = objOne;
+
+objTwo.email = 'one@gmail.com'
+
+// console.log(objOne);
+// console.log(objTwo);
