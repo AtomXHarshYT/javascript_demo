55b9a988d38c5acfdba3bedc9a9a02674f4211f1
AtomicHarshness <155515415+AtomXHarshYT@users.noreply.github.com>
2024-03-22 11:50:53 +0000

    dataTypes learned

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,13 @@
+// Number => 5625648
+// BigInt => 545565456365154
+// String => "characters"
+// Boolean => true/false
+// null => standalone value
+// undefined => not assigned
+// Symbol => mostly use in React
+
+
+
+// Object
+
+// console.log(typeof name); by  this we can identify the type of variables
