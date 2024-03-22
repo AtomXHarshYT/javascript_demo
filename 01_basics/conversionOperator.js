59353c3d0657d95d63601bdd5567205b5d25562b
AtomicHarshness <155515415+AtomXHarshYT@users.noreply.github.com>
2024-03-22 12:03:32 +0000

    convert operation

---
+++ 01_basics/conversionOperator.js
@@ -0,0 +1,15 @@
+let score = "65";
+
+console.log(typeof(score));
+
+let inNum = Number(score);
+console.log(typeof(inNum));
+
+// "65" - 65
+// "65aa" - NaN
+// "true - 1 && false - 0"
+
+//  1 - true
+//  0 - false
+//  "" - false
+//  "harsh" - true
