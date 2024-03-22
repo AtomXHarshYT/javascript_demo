4ba0daced8595a83c23f99810acb4645fc7a7351
AtomicHarshness <155515415+AtomXHarshYT@users.noreply.github.com>
2024-03-22 16:18:42 +0000

    maths learned

---
+++ nums_&_maths.js
@@ -0,0 +1,36 @@
+// ------------------------------------------nums ----------------------------------------------
+const bal = 566;
+// console.log(bal);
+
+const score = new Number(4555);
+// console.log(score);
+
+// console.log(score.toString());
+// console.log(score.toFixed(2));
+
+let st = 'hhhaaarrrsss';
+// console.log(st.slice(0, 5));
+
+const nums = 454545455;
+let nums2 = nums.toLocaleString('en-IN');
+
+// console.log(nums2);
+
+
+// ------------------------------------------maths ----------------------------------------------
+
+// console.log(Math);
+// console.log(Math.abs('-9'));
+// console.log(Math.round('5.5'));
+// console.log(Math.ceil('5.4'));
+// console.log(Math.floor('5.4'));
+// console.log(Math.min(4,5,6,7));
+// console.log(Math.max(4,5,6,7));
+
+
+// console.log((Math.random()*10) + 1);
+
+const sml = 10;
+const bg = 20;
+
+console.log(Math.floor(Math.random() * (bg - sml + 1) + sml));
